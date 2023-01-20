105ba73d19fad34f1bd3a2f22f0821bf3663e720
Maheshdh <36495289+Maheshdh@users.noreply.github.com>
2023-01-21 00:32:48 +0530

    Create 120 : Traingle.py

---
+++ 120 : Traingle.py
@@ -0,0 +1,11 @@
+class Solution:
+    # Time complexity : O(n^2)
+    # Space complexity : O(n)
+    
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [0] * (len(triangle[-1])+1)
+        for row in triangle[::-1]:
+            for i,val in enumerate(row):
+                dp[i] = val+min(dp[i],dp[i+1])
+        
+        return dp[0]
